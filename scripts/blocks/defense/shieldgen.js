f7f5392251b58dd95f7c9e2034a4310f3ffa1526
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 22:10:38 +0700

    h

--- scripts/blocks/defense/shieldgen.js
+++ scripts/blocks/defense/shieldgen.js
@@ -1,1 +1,33 @@
 
+const shieldGen = extendContent(ForceProjector, "shieldgen", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.spinRegion = Core.atlas.find(this.name + "-spin");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name),
+      Core.atlas.find(this.name + "-spin"),
+      Core.atlas.find(this.name + "-top")
+    ]
+  },
+  
+  draw(tile){
+    entity = tile.ent();
+    
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    
+    if(entity.buildup <= 0){
+      Draw.alpha(entity.buildup / breakage * 0.75);
+      Draw.blend(Blending.additive);
+      Draw.rect(this.spinRegion, tile.drawx(), tile.drawy(), entity.totalProgress * 1.5);
+      Draw.blend();
+      Draw.reset();
+    }
+    
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    
+  }
+});
