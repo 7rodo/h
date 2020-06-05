1670c9bd908306de365453ee6250964e592c93ac
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 19:34:33 +0700

    Effect for dirium wall

--- scripts/blocks/walls/dirium.js
+++ scripts/blocks/walls/dirium.js
@@ -1,1 +1,28 @@
+const wallEffect = newEffect(10, e -> {
+  Draw.color(Color.valueOf("8fdbb2", Color.valueOf("7ec49f"), e.fin());
+  Lines.stroke(e.fout() * 1.6);
+  Lines.square(e.x, e.y, 6 + e.fin() * 2);
+}
+                              
+const dirWall = extendContent(Wall, "diriumwall", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name)
+    ]
+  },
+  
+  draw(){
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+  },
+  
+  handleBulletHit(entity, bullet){
+    entity.damage(bullet.damage());
+    
+    Effects.effect(wallEffect, this);
+  }
+});
 
