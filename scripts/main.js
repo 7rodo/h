99e7bf524e3f0219f5017adfb213ee9a8bffd6c2
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 21:50:38 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+//This is the main.js, also known as main.js, it's function is the same as main.js
+
+//Blocka
+
+//Defense
+  require("blocks/defense/shieldgen");
+
+//Walls
+  require("blocks/walls/dirium");
