1478a5c3f38801940d5d8077ab53f210397dd12a
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 20:49:34 +0700

    Update dirium.js

--- scripts/blocks/walls/dirium.js
+++ scripts/blocks/walls/dirium.js
@@ -36,7 +36,7 @@ const dirWall = extendContent(Wall, "diriumwall", {
   handleBulletHit(entity, bullet){
     entity.damage(bullet.damage());
     
-    Effect.effects(wallEffect, this.x, this.y);
+    Effects.effect(wallEffect, this.x, this.y);
   }
 });
 
