30183bd7d3ebb4d79684a5ec6deb13781371af2e
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-05 19:48:29 +0700

    Large dirium wall effect

--- scripts/blocks/walls/dirium.js
+++ scripts/blocks/walls/dirium.js
@@ -1,8 +1,14 @@
-const wallEffect = newEffect(10, e -> {
-  Draw.color(Color.valueOf("8fdbb2", Color.valueOf("7ec49f"), e.fin());
+const wallEffect = newEffect(10, e => {
+  Draw.color(Color.valueOf("8fdbb2"), Color.valueOf("7ec49f"), e.fin());
   Lines.stroke(e.fout() * 1.6);
-  Lines.square(e.x, e.y, 6 + e.fin() * 2);
-}
+  Lines.square(e.x, e.y, 5 + e.fin() * 2);
+});
+                             
+const wallEffectLarge = newEffect(10, e => {
+  Draw.color(Color.valueOf("8fdbb2"), Color.valueOf("7ec49f"), e.fin());
+  Lines.stroke(e.fout() * 1.6);
+  Lines.square(e.x, e.y, 10 + e.fin() * 2);
+});                            
                               
 const dirWall = extendContent(Wall, "diriumwall", {
   load(){
@@ -23,6 +29,28 @@ const dirWall = extendContent(Wall, "diriumwall", {
     entity.damage(bullet.damage());
     
     Effects.effect(wallEffect, this);
+  }
+});
+
+const dirWallLarge = extendContent(Wall, "diriumwalllarge", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name)
+    ]
+  },
+  
+  draw(){
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+  },
+  
+  handleBulletHit(entity, bullet){
+    entity.damage(bullet.damage());
+    
+    Effects.effect(wallEffectLarge, this);
   }
 });
 
