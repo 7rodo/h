31c38b608f9dbf4a1ff079f20f6c48941739a62a
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-06 21:38:34 +0700

    Update tesla.js

--- scripts/blocks/turrets/tesla.js
+++ scripts/blocks/turrets/tesla.js
@@ -18,8 +18,8 @@ const teslaShoot = newEffect(21, e => {
 const teslaLaser = extend(BasicBulletType, {
   draw(b){
     const colors = [color1, color2, Color.valueOf("ffffff")];
-    const tscales = [1, 0.7, 0.5, 0.2];
-    const lenscales = [1, 1.1, 1.13, 1.14];
+    const tscales = [0.9, 0.6, 0.4, 0.1];
+    const lenscales = [0.9, 1, 1.1, 1.12];
     const length = 95;
 
     f = Mathf.curve(b.fin(), 0, 0.2);
@@ -38,7 +38,7 @@ const teslaLaser = extend(BasicBulletType, {
 });
 
 teslaLaser.lifetime = 16;
-teslaLaser.pierce = true;
+teslaLaser.pierce = false;
 teslaLaser.hitSize = 4;
 teslaLaser.collidesAir = false;
 teslaLaser.damage = 8
