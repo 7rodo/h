0319cacc35def2fe32a30422435495cefd2265e5
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-06 13:53:13 +0700

    Require lava.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 //This is the main.js, also known as main.js, it's function is the same as main.js
 
 //Blocka
+  //Defense
+    require("blocks/defense/shieldgen");
 
-//Defense
-  require("blocks/defense/shieldgen");
+  //Walls
+    require("blocks/walls/dirium");
 
-//Walls
-  require("blocks/walls/dirium");
+//Resources & Materials
+
+  //Liquids
+    require("liquids/lava");
