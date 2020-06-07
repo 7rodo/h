021b526867bd21d5b48e6e9e31edb141995aeb1b
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-07 17:23:05 +0700

    Update shieldwall.js

--- scripts/blocks/walls/shieldwall.js
+++ scripts/blocks/walls/shieldwall.js
@@ -38,7 +38,11 @@ const shieldWall = extendContent(Wall, "shieldwall", {
   },
   
   handleBulletHit(entity, bullet){
-      entity.damage(bullet.damage() / 3);  
+    if(entity.power.status > 0.001){  
+      entity.damage(bullet.damage() / 3); 
+    } else {
+        entity.damage(bullet.damage());
+      }
     
     if(entity != null && bullet != null){
       Effects.effect(wallEffect, entity.x, entity.y, entity.rotation - 90);
