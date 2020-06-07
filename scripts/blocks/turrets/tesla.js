b0197c7f5642109385e59dffba1c00d42309e426
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-07 13:35:51 +0700

    Update tesla.js

--- scripts/blocks/turrets/tesla.js
+++ scripts/blocks/turrets/tesla.js
@@ -52,7 +52,7 @@ teslaLaser.smokeEffect = Fx.none;
 teslaLaser.despawnEffect = Fx.none;
 teslaLaser.hitEffect = Fx.none;
 teslaLaser.lightining = 4;
-teslaLaser.lightningLength = 7
+teslaLaser.lightningLength = 5
 
 
 const tesla = extendContent(PowerTurret, "tesla", {});
