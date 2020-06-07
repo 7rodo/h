6da70456c2d659d77cf32876683cad3a88f93b34
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-07 19:23:20 +0700

    hitSize

--- scripts/blocks/turrets/tesla.js
+++ scripts/blocks/turrets/tesla.js
@@ -39,7 +39,7 @@ const teslaLaser = extend(BasicBulletType, {
 
 teslaLaser.lifetime = 60;
 teslaLaser.pierce = true;
-teslaLaser.hitSize = 4;
+teslaLaser.hitSize = 16;
 teslaLaser.collides = true;
 teslaLaser.collidesGround = true;
 teslaLaser.collidesAir = false;
