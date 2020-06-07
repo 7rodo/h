c309ca5d397dec9372beb83f87d1163cfe666c8c
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-07 19:18:24 +0700

    Update tesla.js

--- scripts/blocks/turrets/tesla.js
+++ scripts/blocks/turrets/tesla.js
@@ -37,7 +37,7 @@ const teslaLaser = extend(BasicBulletType, {
   }
 });
 
-teslaLaser.lifetime = 45;
+teslaLaser.lifetime = 15;
 teslaLaser.pierce = true;
 teslaLaser.hitSize = 4;
 teslaLaser.collides = true;
