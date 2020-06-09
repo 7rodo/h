64892d74388e343a889ef7ea490e15f7e4e1e105
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-09 18:17:26 +0700

    Update and rename laser.js to pulse.js

---
+++ scripts/blocks/turrets/pulse.js
@@ -0,0 +1,29 @@
+const pulse = extendContent(PowerTurret, "pulse", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.baseRegion = Core.atlas.find(this.name + "-base");
+    this.spinRegion = Core.atlas.find(this.name + "-spin");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name + "-base"),
+      Core.atlas.find(this.name + "-spin"),
+      Core.atlas.find(this.name)
+    ]
+  },
+  
+  draw(tile){
+    Draw.rect(this.baseRegion, tile.drawx(), tile.drawy());
+    Draw.rect(this.spinRegion, tile.drawx(), tile.drawy(), tile.entity.rotation() += 8);
+  },
+  
+  update(tile){
+    
+  },
+  
+  shouldTurn(tile){
+    
+  }
+  
+});
