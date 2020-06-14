1011ba89a1275d3150636fcce07c33f66c96aad1
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-14 12:13:36 +0700

    init

--- scripts/blocks/turrets/tesla.js
+++ scripts/blocks/turrets/tesla.js
@@ -30,12 +30,12 @@ const teslaHit = newEffect(21, e => {
   Fill.circle(e.x, e.y, e.fin() * 4);
 });
 
-const colors1 = [color1, color2, Color.valueOf("ffffff")];
+//const colors1 = [color1, color2, Color.valueOf("ffffff")];
 //const tscales1 = [0.3, 0.2, 0.15, 0.1];
 //const lenscales1 = [1, 1.1, 1.13, 1.14];
-const length1 = 3;
+//const length1 = 3;
 
-const teslaFrag = extend(BasicBulletType, {
+/*const teslaFrag = extend(BasicBulletType, {
   init(b){
     if(!b){
       return;
@@ -58,7 +58,7 @@ teslaFrag.collidesTiles = false;
 teslaFrag.shootEffect = Fx.none;
 teslaFrag.smokeEffect = Fx.none;
 teslaFrag.despawnEffect = Fx.none;
-teslaFrag.hitEffect = Fx.none;
+teslaFrag.hitEffect = Fx.none;*/
 
 const colors = [color1, color2, Color.valueOf("ffffff")];
 const tscales = [0.5, 0.4, 0.3, 0.2];
@@ -68,6 +68,13 @@ const length = 99;
 const teslaLaser = extend(BasicBulletType, {
   update(b){
     Damage.collideLine(b, b.getTeam(), Fx.none, b.x, b.y, b.rot(), length, true);
+  },  
+  
+  init(b){
+    if(!b){
+      return;
+    }
+    Lightning.create(b.getTeam(), color1, this.damage, b.x, b.y, b.rot(), 12);
   },  
   
   draw(b){
@@ -103,13 +110,13 @@ teslaLaser.hitEffect = Fx.none;
 /*
 teslaLaser.lightining = 4;
 teslaLaser.lightingLength = 6;
-*/
+*//*
 teslaLaser.fragBullet = teslaFrag;
 teslaLaser.fragBullets = 2;
 teslaLaser.fragVelocityMin = 0;
 teslaLaser.fragVelocityMax = 0.2;
-//teslaLaser.hitShake = 0.2;
-
+teslaLaser.hitShake = 0.2;
+*/
 
 const tesla = extendContent(PowerTurret, "tesla", {});
 
