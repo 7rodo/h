c296cd86074e06980668cdbb4442b94f9fc66977
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-15 17:52:23 +0700

    h

---
+++ scripts/blocks/production/crucible.js
@@ -0,0 +1,24 @@
+const cru = extendContent(GenericCrafter, "crucible", {
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.spinRegion = Core.atlas.find(this.name + "-spin");
+    this.topRegion = Core.atlas.find(this.name + "-top");
+  },
+  
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name),
+      Core.atlas.find(this.name + "-spin"),
+      Core.atlas.find(this.name + "-top")
+    ]
+  },
+  
+  draw(tile){
+    entity = tile.ent()
+
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    Draw.rect(this.spinRegion, tile.drawx(), tile.drawy(), entity.totalProgress * 2);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    }
+  }
+});
