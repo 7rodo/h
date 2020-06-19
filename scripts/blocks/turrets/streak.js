2fb4e15f3dc9120db4f6427461f907a3623054d7
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-06-19 13:57:34 +0700

    hhhhh

---
+++ scripts/blocks/turrets/streak.js
@@ -0,0 +1,33 @@
+const tmpColor = new Color();
+const colors = {Color.valueOf("5757c2"), Color.valueOf("6a75c4"), Color.valueOf("8ca4f5"), Color.white};
+const tscales = {1, 0.7, 0.5, 0.2};
+const strokes = {2, 1.5, 1, 0.3};
+const lenscales = {1, 1.12, 1.15, 1.17};
+const length = 210;
+
+const streakLaser = extend(BssicBulletType, {
+  update(b){
+    if(b.timer.get(1, 5)){
+      Damage.collideLine(b, b.getTeam(), hitEffect, b.x, b.y, b.rot(), length, true);
+    }
+  },
+  
+  draw(b){
+    var baseLen = (length) * b.fout();
+  
+    Lines.lineAngle(b.x, b.y, b.rot(), baseLen);
+      for(var s = 0; s < colors.length; s++){
+        Draw.color(tmpColor.set(colors[s]).mul(1 + Mathf.absin(Time.time(), 1, 0.1)));
+         for(var i = 0; i < tscales.length; i++){
+           Tmp.v1.trns(b.rot() + 180, (lenscales[i] - 1) * 35);
+           Lines.stroke((9 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
+           Lines.lineAngle(b.x + Tmp.v1.x, b.y + Tmp.v1.y, b.rot(), baseLen * lenscales[i], CapStyle.none);
+         }
+      }
+    Draw.reset();
+  }
+});
+
+const streak = extendContent(LaserTurret, "streak", {});
+
+streak.shootType = streakLaser;
