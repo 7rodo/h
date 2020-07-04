4d3a5384f782fa149fd570b79d8fc1a5e682ea00
Gdeft <gdeftno@gmail.com>
2020-07-04 18:54:38 +0700

    order 66 for now

--- scripts/order66.js
+++ scripts/order66.js
@@ -1,3 +1,9 @@
+const reverseCircle = newEffect(20, e => {
+  Draw.color(Pal.lancerLaser, Color.white, e.fin())
+  Lines.stroke(e.fin() * 3)
+  Lines.circle(e.x, e.y, e.fout() * 65)
+});
+
 const order = extendContent(Block, "order66", {
   buildConfiguration(tile, table){
     table.addImageButton(Icon.eye, Styles.clearTransi, run(() => {
@@ -13,8 +19,12 @@ const order = extendContent(Block, "order66", {
 
   update(tile){
     if(tile.entity.power.status > 0.001){
+      if(Mathf.chance(0.005)){
+        Effects.effect(reverseCircle, tile.drawx(), tile.drawy())
+      }
+
       if(Mathf.chance(0.04)){
-        Lightning.create(Team.crux, Pal.lancerLaser, Mathf.random(2.7, 9.6), tile.drawx() + Mathf.random(-60, 60), tile.drawy() + Mathf.random(-60, 60), Mathf.random(360), Mathf.random(2, 11))
+        Lightning.create(Team.crux, Pal.lancerLaser, Mathf.random(2.7, 9.6), tile.drawx() + Mathf.random(-60, 60), tile.drawy() + Mathf.random(-60, 60), Mathf.random(360), Mathf.random(4, 11))
       }
     }
   }
