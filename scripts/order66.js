d08f713f8eeab571b4c386a07c932f43910a8de6
Gdeft <gdeftno@gmail.com>
2020-07-04 15:26:22 +0700

    Lightning chance

--- scripts/order66.js
+++ scripts/order66.js
@@ -7,8 +7,16 @@ const order = extendContent(Block, "order66", {
 
   configured(tile, value){
     if(tile.entity.cons.valid() && tile.entity.power.status > 0.001){
-      tile.entity.cons.trigger()
+      Damage.damage(tile.drawx(), tile.drawy(), 1250, Mathf.random(40, 160))
+    }
+  },
+
+  update(tile){
+    if(tile.entity.power.status > 0.001){
+      if(Mathf.chance(0.04)){
+        Lightning.create(Team.crux, Pal.lancerLaser, Mathf.random(2.7, 9.6), tile.drawx() + Mathf.random(-60, 60), tile.drawy() + Mathf.random(-60, 60), Mathf.random(360), Mathf.random(2, 11))
+      }
     }
   }
-})
+});
 
