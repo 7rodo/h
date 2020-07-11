0d48fb0139ad1a60dcdf4ed9c5edbdc4e8ba0e6f
Gdeft <gdeftno@gmail.com>
2020-07-11 18:49:33 +0700

    Summoner

--- scripts/units/summoner.js
+++ scripts/units/summoner.js
@@ -1,7 +1,12 @@
 const ulib = require("ulib");
 
+const summonEffect = newEffect(10, e => {
+  Draw.color(Pal.lancerLaser);
+  Lines.square(e.x, e.y, 10 * e.fin(), 45);
+})
+
 const deadEffect = newEffect(12, e => {
-  Draw.color(Pal.lancerLaser, Color.white, e.fin());
+  Draw.color(Pal.lancerLaser);
   Lines.circle(e.x, e.y, 40 * e.fin());
 });
 
@@ -20,7 +25,7 @@ orb.damage = 0;
 orb.hitEffect = Fx.none;
 orb.shootEffect = Fx.none;
 orb.despawnEffect = Fx.none;
-orb.shootEffect = Fx.none;
+orb.shootEffect = summonEffect;
 orb.smokeEffect = Fx.none;
 
 const summonerWeapon = extendContent(Weapon, "summoner-equip", {
@@ -29,7 +34,7 @@ const summonerWeapon = extendContent(Weapon, "summoner-equip", {
   }
 });
 
-summonerWeapon.reload = 600;
+summonerWeapon.reload = 360;
 summonerWeapon.shootSound = Sounds.wave;
 summonerWeapon.bullet = orb;
 
@@ -38,8 +43,20 @@ const summoner = extendContent(UnitType, "summoner", {
     this.weapon.load();
     this.region = Core.atlas.find(this.name);
     this.baseRegion = Core.atlas.find(this.name + "-base");
-    this.legRegion = Core.atlas.find(this.name + "-leg");
+    this.legRegion = Core.atlas.find("clear");
   }
-}, prov(() => extend(GroundUnit, {})));
+});
+
+summoner.create(prov(() => extend(GroundUnit, {
+    onDeath(){
+      Effects.effect(deadEffect, this);
+      Effects.shake(2, 2, this);
+
+      Sounds.bang.at(this);
+      this.item.amount = 0;
+      this.drownTime = 0;
+      Events.fire(EventType.UnitDestroyEvent(this));
+    }
+})));
 
 summoner.weapon = summonerWeapon;
